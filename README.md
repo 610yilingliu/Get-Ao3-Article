d8ae8bbcc0792d34408fcef3b672ec168f40067e
610yilingliu <yilingliu1994@gmail.com>
2020-03-02 15:26:21 +0800

    Add getnotes(), getsummary() in article.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Get-Ao3-Article
-批量获取AO3文章并保存为长截图（Get Ao3 articles through auther name or tags, save as image）
+批量获取AO3文章并保存为文档（Get Ao3 articles through auther name or tags, save as image）
+
+原定保存为长图，但因为selenium需要chrome driver，得额外安装所以还是保存为文档了
+
+使用正则表达式清理网页非常容易出问题（发文格式不同，对应的html戳也不同），目前尚在测试排查bug
+
+目前article.py是bug重灾区
--- pkg/article.py
+++ pkg/article.py
@@ -30,25 +30,57 @@ class article(object):
         pattern = re.compile(r'<a rel=\"author\" href=\"\/users\/(.*?)/')
         author = pattern.search(html).groups()[0]
         return author
+    
+    def getsummary(self):
+        html = self.__html
+        pattern = re.compile(r'<h3 class=\"heading\">Summary\:<\/h3>([\s\S]*)<\/blockquote>')
+        search_result = pattern.search(html)
+        if search_result != None:
+            summary = search_result.groups()[0]
+            return summary
+        return None
+
+    def getnotes(self):
+        '''return list'''
+        html = self.__html
+        pattern = re.compile(r'<h3 class=\"heading\">Notes\:<\/h3>([\s\S]*?)<\/blockquote>')
+        search_result = pattern.findall(html)
+        return search_result
 
     def getcontent(self):
         html = self.__html
-        pattern = re.compile(r'userstuff\">([\s\S]*)<\!-- end cache -->')
+        pattern = re.compile(r'<\!--main content-->([\s\S]*)((?=<\!--\/chapter-->)|(?=<\!--\/main-->))')
         search_result = pattern.search(html)
-        # if is in Chapter
+        # not good enough.((?<=<\!--main content-->)|(?<=<\!--chapter content-->))([\s\S]*)((?=<\!--\/chapter-->)|(?=<\!--\/main-->)) works in regexr but does not work here
         if search_result == None:
-            pattern2 = re.compile(r'</h3>\n([\s\S]*)<\!--\/main-->')
-            content = pattern2.search(html).groups()[0]
-        else:
-            content = search_result.groups()[0]
-        replace_dict = {
-            '<p>':'',
-            '</p>': '\n',
-            '<br/>': '\n',
-            '<div>': '',
-            '</div>':'',
-            '<p dir=\"ltr\">':'',
-        }
-        for key in replace_dict.keys():
-            content = content.replace(key, replace_dict[key])
+            pattern = re.compile(r'<\!--chapter content-->([\s\S]*)((?=<\!--\/chapter-->)|(?=<\!--\/main-->))')
+            search_result = pattern.search(html)
+        content = search_result.groups()[0]
         return content
+
+def cleaner(text):
+    replace_dict = {
+        '<!--main content-->':'\n',
+        '<!--chapter content-->':'\n',
+        '<div class=\"userstuff module\" role=\"article\">': '',
+        '<div class=\"userstuff\">': '',
+        '<h3 class=\"landmark heading\" id=\"work\">':'',
+        '<div id=\"chapters\" role=\"article\">':'',
+        '</h3>': '\n',
+        '<p>':'',
+        '</p>': '\n',
+        '<br/>': '\n',
+        '<div>': '',
+        '</div>':'',
+        '<p dir=\"ltr\">':'',
+        '<blockquote class=\"userstuff\">': '',
+        '</blockquote>':'\n',
+        '<!-- end cache -->':'\n'
+    }
+    if text!= None:
+        for key in replace_dict.keys():
+            text = text.replace(key, replace_dict[key])
+        return text.strip()
+    else:
+        return None
+    
